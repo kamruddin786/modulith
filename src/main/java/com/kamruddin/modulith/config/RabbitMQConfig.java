b49911b6727f75625f02866040bbcb375fafc068
Kamruddin Mansoori <KM2170@zebra.com>
2025-10-15 12:54:23 +0530

    refactor: Remove unused DistributedLockConfig class and clean up RabbitMQConfig imports

--- src/main/java/com/kamruddin/modulith/config/RabbitMQConfig.java
+++ src/main/java/com/kamruddin/modulith/config/RabbitMQConfig.java
@@ -3,7 +3,6 @@ package com.kamruddin.modulith.config;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
